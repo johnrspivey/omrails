e6d0b28b73b05e529fd873db6641f47f6f5e2804
John Spivey <johnrspivey@gmail.com>
2019-11-29 13:22:42 -0600

    changed some text to better reflect purpose of this app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	root 'pages#home'
     get 'about' => 'pages#about'
-    get 'contact_us' => 'pages#contact_us'
+    get 'contactus' => 'pages#contactus'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
